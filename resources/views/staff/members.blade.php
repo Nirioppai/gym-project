78b27c0505feff0c9a1c966b04c0781efafeb7df
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2022-04-05 20:07:55 +0800

    landing page banner finish

--- resources/views/staff/members.blade.php
+++ resources/views/staff/members.blade.php
@@ -33,6 +33,21 @@
                     <div class="tab-content" id="myTabContent">
                         <div class="tab-pane fade show active" id="member-list" role="tabpanel"
                             aria-labelledby="member-list-tab">
+                            <form>
+                                <div class="row">
+                                    <div class="col-md">
+                                        <div class="form-group">
+                                            <div class="input-group ">
+                                                <span class="input-group-text"><i class="ni ni-zoom-split-in"></i></span>
+                                                <input class="form-control" placeholder="Search" type="text">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md d-flex justify-content-end">
+                                        <button type="button" class="btn btn-primary btn-sm">Add</button>
+                                    </div>
+                                </div>
+                            </form>
                             <div class="table-responsive">
                                 <table class="table align-items-center ">
                                     <tbody>
@@ -311,76 +326,5 @@
             </div>
         </div>
 
-    </div>
-
-
-    <div class="row mt-4">
-        <div class="col-lg-7 mb-lg-0 mb-4">
-            <div class="card z-index-2 h-100">
-                <div class="card-header pb-0 pt-3 bg-transparent">
-                    <h6 class="text-capitalize">Sales overview</h6>
-                    <p class="text-sm mb-0">
-                        <i class="fa fa-arrow-up text-success"></i>
-                        <span class="font-weight-bold">4% more</span> in 2021
-                    </p>
-                </div>
-                <div class="card-body p-3">
-                    <div class="chart">
-                        <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-5">
-            <div class="card card-carousel overflow-hidden h-100 p-0">
-                <div id="carouselExampleCaptions" class="carousel slide h-100" data-bs-ride="carousel">
-                    <div class="carousel-inner border-radius-lg h-100">
-                        <div class="carousel-item h-100 active" style="background-image: url('{{ asset('img/carousel-1.jpg') }}');
-                                                                background-size: cover;">
-                            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-                                <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                                    <i class="ni ni-camera-compact text-dark opacity-10"></i>
-                                </div>
-                                <h5 class="text-white mb-1">Get started with Argon</h5>
-                                <p>There’s nothing I really wanted to do in life that I wasn’t able to get good
-                                    at.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item h-100" style="background-image: url('{{ asset('img/carousel-2.jpg') }}');
-                                                                background-size: cover;">
-                            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-                                <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                                    <i class="ni ni-bulb-61 text-dark opacity-10"></i>
-                                </div>
-                                <h5 class="text-white mb-1">Faster way to create web pages</h5>
-                                <p>That’s my skill. I’m not really specifically talented at anything except for
-                                    the ability to learn.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item h-100" style="background-image: url('{{ asset('img/carousel-3.jpg') }}');
-                                                                background-size: cover;">
-                            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-                                <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                                    <i class="ni ni-trophy text-dark opacity-10"></i>
-                                </div>
-                                <h5 class="text-white mb-1">Share with us your design tips!</h5>
-                                <p>Don’t be afraid to be wrong because you can’t learn anything from a
-                                    compliment.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
